cb9b0a63d769642c75f7c6b3246f1bb23fac28b6
Damien Eversmann <damien.eversmann@gmail.com>
2021-07-02 14:19:51 -0500

    Removing unused legacy files

--- cardentemp/crabfeed2013/footer.php
+++
@@ -1,8 +0,0 @@
-	</div>
-	<div id="footer">
-		<p>Please report any site problems to <a href="mailto:cardencrabfeed@gmail.com?subject=web-site-issue">cardencrabfeed@gmail.com</a></p>
-		<p style="font-size:.75em;">Event sponsored by: Carden School of Sacramento Parents' Club</p>
-	</div>
-	</div>
-</body>
-</html>
